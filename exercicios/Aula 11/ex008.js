67ef00989771d58bc05296f111dd7da0d8085c14
Luana Stanicheski Smokovicz <121254943+lussmokovicz@users.noreply.github.com>
2023-01-04 09:54:07 -0300

    update

---
+++ exercicios/Aula 11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel}`)
+
+//CRIAR CONDIÇÃO//
+if (vel>=60.0){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança')
---
+++ exercicios/Aula 11/ex009.js
@@ -0,0 +1,9 @@
+//CONDIÇÃO COMPOSTA//
+
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if(país == 'Brasil'){
+    console.log('Você é Brasileiro')
+}else{
+    console.log('Você é Estrangeiro!')
+}
