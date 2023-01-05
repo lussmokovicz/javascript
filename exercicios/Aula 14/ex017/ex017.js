50cfaf7600fce454a360a78ecb56b0cfad3921a6
Luana Stanicheski Smokovicz <121254943+lussmokovicz@users.noreply.github.com>
2023-01-05 13:32:23 -0300

    update

---
+++ exercicios/Aula 14/ex017/ex017.js
@@ -0,0 +1,22 @@
+console.log('Vai começar...')
+for (var c= 1; c<5 ; c++ ){
+    console.log('Tudo bem?')
+}
+console.log('Fim!')
+
+
+
+
+
+/*var c = 1
+do{
+    console.log('Tudo bem?')
+    c++
+}while (c<5) */
+
+
+/*var c = 1
+while (c<5){
+    console.log('Tudo bem?')
+    c++
+} */
